58c0b1b004acb705346d9e5838d72b3a1699850e
Endyrian <nathanenderman@gmail.com>
2025-10-30 17:15:48 -0700

    Started function to test where a pentomino can go

--- pentomino_solver.cpp
+++ pentomino_solver.cpp
@@ -4,12 +4,37 @@
 #include <vector>
 #include <iostream>
 
+// Returns a vector of all possible puzzle grid arrangements which can be
+// obtained by adding `piece` in a valid location to one of the grids in
+// `old_grids`
 std::vector<std::array<std::array<char, 8>, 8>> addPentomino(
-        std::vector<std::array<std::array<char, 8>, 8>> grids,
+        std::vector<std::array<std::array<char, 8>, 8>> old_grids,
         Pentomino piece) {
-    std::vector<std::array<std::array<char, 8>, 8>> added_grids;
-    // TODO: test all possible placements for the piece
-    return added_grids;
+    std::vector<std::array<std::array<char, 8>, 8>> new_grids;
+    int rotations = piece.getRotations();
+    bool symmetrical = piece.isSymmetrical();
+    int height = piece.getHeight();
+    int width = piece.getWidth();
+    for (auto &&grid : old_grids) {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                for (int i = 0; i < rotations; i++) {
+                    if (i % 2 == 0 && x + height < 8 && y + width < 8) {
+                        /* code */
+                    } else if (y + height < 8 && x + width < 8) {
+                        /* code */
+                    }
+                    
+                    // TODO: test all possible placements for the piece
+                }
+                
+            }
+            
+        }
+
+    }
+    
+    return new_grids;
 }
 
 int main(int argc, char const *argv[]) {
